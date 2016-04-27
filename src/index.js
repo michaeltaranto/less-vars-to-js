2aa4eafc566591f43f35f633a94b5bf5450595af
Michael Taranto <mtaranto@seek.com.au>
2016-04-28 08:51:43 +1000

    Remove redundant less imports check

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ export default sheet => {
 
   sheet.match(/@(.*:[^;]*)/g)
     .forEach(variable => {
-      if (variable.indexOf('@import') === 0) {
-        return;
-      }
       const definition = variable.split(/:\s*/);
       lessVars[definition[0]] = definition[1];
     });
