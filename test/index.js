409e0f1654a7156408a295793f1aed6e3e14125e
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-08-06 21:18:08 -0400

    Convert the tests

--- test/index.js
+++ test/index.js
@@ -1,140 +1,101 @@
 import lessVarsToJS from '../src';
 import { expect } from 'chai';
 
-const cases = [
-  {
-    should: 'should read variables that are hex values',
-    input: `
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should read variables that are named colours',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that are hex values', () => expect(lessVarsToJS(`
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      @blue: lightblue;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': 'lightblue',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should read variables that reference other variables',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that are named colours', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: lightblue;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': 'lightblue',
+  '@pink': '#e60278'
+}));
 
-      @blue: lightblue;
-      @pink: @blue;
-    `,
-    output: {
-      '@blue': 'lightblue',
-      '@pink': '@blue'
-    }
-  },
-  {
-    should: 'should ignore comments',
-    input: `
-      // colour palette
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore import statements',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that reference other variables', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: lightblue;
+  @pink: @blue;
+`)).to.deep.equal({
+  '@blue': 'lightblue',
+  '@pink': '@blue'
+}));
 
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore rules',
-    input: `
-      @import (reference) 'theme';
+it('should ignore comments', () => expect(lessVarsToJS(`
+  // colour palette
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      @blue: #0d3880;
+it('should ignore import statements', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      .element {
-        color: @foreground;
-      }
-
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore include variables from within',
-    input: `
-      @import (reference) 'theme';
-
-      @blue: #0d3880;
-
-      .element {
-        @foreground: black;
-        color: @foreground;
-      }
-
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@foreground': 'black',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should not break in file with no variables',
-    input: `
-      @import (reference) 'theme';
-
-      .element {
-        color: black;
-      }
-    `,
-    output: {}
-  },
-  {
-    should: 'should trim variable names',
-    input: `
-      @blue : #0d3880;
-    `,
-    output: {
-      '@blue': '#0d3880'
-    }
-  },
-  {
-    should: 'should read variables that are url',
-    input: `
-      @icon-url : "https://xxx.com:8080/t/font";
-    `,
-    output: {
-      '@icon-url': 'https://xxx.com:8080/t/font'
-    }
+it('should ignore rules', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  
+  .element {
+    color: @foreground;
   }
-];
+  
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-describe('Given the contents of a less file', () => {
-  cases.forEach(({ should, input, output }) => {
-    it(should, () => expect(lessVarsToJS(input)).to.deep.equal(output));
-  });
-});
+it('should ignore include variables from within', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  
+  .element {
+    @foreground: black;
+    color: @foreground;
+  }
+  
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@foreground': 'black',
+  '@pink': '#e60278'
+}));
+
+it('should not break in file with no variables', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  .element {
+    color: black;
+  }
+`)).to.deep.equal({}));
+
+it('should trim variable names', () => expect(lessVarsToJS(`
+    @blue : #0d3880;
+`)).to.deep.equal({
+  '@blue': '#0d3880'
+}));
+
+it('should read variables that are url', () => expect(lessVarsToJS(`
+  @icon-url : "https://xxx.com:8080/t/font";
+`)).to.deep.equal({
+  '@icon-url': 'https://xxx.com:8080/t/font'
+}));
