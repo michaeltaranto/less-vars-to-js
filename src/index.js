a1d4ba3528e81abadd18fce4830549fc329b0efc
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-08-06 21:00:48 -0400

    Init
    - The variables will be resolved from other values in the same file, or from a supplied object.
    - The commented values be ignored.
    - The resulting object will not contain the `@` character.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-export default sheet => {
+import stripComments from 'strip-json-comments';
+
+const varRgx = /^[@$]/;
+const followVar = (value, lessVars, constants) => {
+  if (varRgx.test(value)) {
+    // value is a variable
+    return followVar(lessVars[value] || constants[value.substring(1)]);
+  }
+  return value;
+};
+
+export default (sheet, constants = {}) => {
   const lessVars = {};
-  const matches = sheet.match(/@(.*:[^;]*)/g) || [];
+  const matches = stripComments(sheet).match(/[@$](.*:[^;]*)/g) || [];
 
   matches.forEach(variable => {
     const definition = variable.split(/:\s*/);
-    lessVars[definition[0].replace(/['"]+/g, '').trim()] = definition.splice(1).join(':');
+    let value = definition.splice(1).join(':');
+    value = value.trim().replace(/^["'](.*)["']$/, '$1');
+    lessVars[definition[0].replace(/['"]+/g, '').trim()] = value;
   });
 
-  return lessVars;
+  Object.keys(lessVars).forEach(key => {
+    const value = lessVars[key];
+    lessVars[key] = followVar(value, lessVars, constants);
+  });
+
+  const transformKey = key => key.replace(varRgx, '');
+
+  return Object.keys(lessVars).reduce((prev, key) => {
+    prev[transformKey(key)] = lessVars[key];
+    return prev;
+  }, {});
 };
