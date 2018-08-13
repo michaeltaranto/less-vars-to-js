7844ed3d135202ee343d7d607d875c798b705db3
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2018-08-13 19:05:27 -0400

    feat(options): Support comments and resolving variables (#9)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-export default sheet => {
-  const lessVars = {};
-  const matches = sheet.match(/@(.*:[^;]*)/g) || [];
+import stripComments from 'strip-json-comments';
+
+const varRgx = /^[@$]/;
+const followVar = (value, lessVars, dictionary) => {
+  if (varRgx.test(value)) {
+    // value is a variable
+    return followVar(lessVars[value] || dictionary[value.replace(varRgx, '')]);
+  }
+  return value;
+};
+
+export default (sheet, options = {}) => {
+  const { dictionary = {}, resolveVariables = false, stripPrefix = false } = options;
+  let lessVars = {};
+  const matches = stripComments(sheet).match(/[@$](.*:[^;]*)/g) || [];
 
   matches.forEach(variable => {
     const definition = variable.split(/:\s*/);
-    lessVars[definition[0].replace(/['"]+/g, '').trim()] = definition.splice(1).join(':');
+    let value = definition.splice(1).join(':');
+    value = value.trim().replace(/^["'](.*)["']$/, '$1');
+    lessVars[definition[0].replace(/['"]+/g, '').trim()] = value;
   });
+
+  if (resolveVariables) {
+    Object.keys(lessVars).forEach(key => {
+      const value = lessVars[key];
+      lessVars[key] = followVar(value, lessVars, dictionary);
+    });
+  }
+
+  if (stripPrefix) {
+    const transformKey = key => key.replace(varRgx, '');
+
+    lessVars = Object.keys(lessVars).reduce((prev, key) => {
+      prev[transformKey(key)] = lessVars[key];
+      return prev;
+    }, {});
+  }
 
   return lessVars;
 };
--- test/index.js
+++ test/index.js
@@ -1,140 +1,159 @@
 import lessVarsToJS from '../src';
 import { expect } from 'chai';
 
-const cases = [
-  {
-    should: 'should read variables that are hex values',
-    input: `
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should read variables that are named colours',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that are hex values', () => expect(lessVarsToJS(`
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      @blue: lightblue;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': 'lightblue',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should read variables that reference other variables',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that are named colours', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: lightblue;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': 'lightblue',
+  '@pink': '#e60278'
+}));
 
-      @blue: lightblue;
-      @pink: @blue;
-    `,
-    output: {
-      '@blue': 'lightblue',
-      '@pink': '@blue'
-    }
-  },
-  {
-    should: 'should ignore comments',
-    input: `
-      // colour palette
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore import statements',
-    input: `
-      @import (reference) 'theme';
+it('should read variables that reference other variables', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: lightblue;
+  @pink: @blue;
+`)).to.deep.equal({
+  '@blue': 'lightblue',
+  '@pink': '@blue'
+}));
 
-      @blue: #0d3880;
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore rules',
-    input: `
-      @import (reference) 'theme';
+it('should resolve variables that reference other variables', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: lightblue;
+  @pink: @blue;
+`, { resolveVariables: true })).to.deep.equal({
+  '@blue': 'lightblue',
+  '@pink': 'lightblue'
+}));
 
-      @blue: #0d3880;
+it('should ignore comments', () => expect(lessVarsToJS(`
+  // colour palette
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      .element {
-        color: @foreground;
-      }
+it('should ignore variables in comments', () => expect(lessVarsToJS(`
+  @blue: #0d3880; // Comment @blue: blue
+  // @blue: blue;
+  /* @blue: blue; */
+`)).to.deep.equal({
+  '@blue': '#0d3880'
+}));
 
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should ignore include variables from within',
-    input: `
-      @import (reference) 'theme';
+it('should ignore import statements', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-      @blue: #0d3880;
-
-      .element {
-        @foreground: black;
-        color: @foreground;
-      }
-
-      @pink: #e60278;
-    `,
-    output: {
-      '@blue': '#0d3880',
-      '@foreground': 'black',
-      '@pink': '#e60278'
-    }
-  },
-  {
-    should: 'should not break in file with no variables',
-    input: `
-      @import (reference) 'theme';
-
-      .element {
-        color: black;
-      }
-    `,
-    output: {}
-  },
-  {
-    should: 'should trim variable names',
-    input: `
-      @blue : #0d3880;
-    `,
-    output: {
-      '@blue': '#0d3880'
-    }
-  },
-  {
-    should: 'should read variables that are url',
-    input: `
-      @icon-url : https://xxx.com:8080/t/font;
-    `,
-    output: {
-      '@icon-url': 'https://xxx.com:8080/t/font'
-    }
+it('should ignore rules', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  
+  .element {
+    color: @foreground;
   }
-];
+  
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@pink': '#e60278'
+}));
 
-describe('Given the contents of a less file', () => {
-  cases.forEach(({ should, input, output }) => {
-    it(should, () => expect(lessVarsToJS(input)).to.deep.equal(output));
-  });
-});
+it('should ignore include variables from within', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  @blue: #0d3880;
+  
+  .element {
+    @foreground: black;
+    color: @foreground;
+  }
+  
+  @pink: #e60278;
+`)).to.deep.equal({
+  '@blue': '#0d3880',
+  '@foreground': 'black',
+  '@pink': '#e60278'
+}));
+
+it('should not break in file with no variables', () => expect(lessVarsToJS(`
+  @import (reference) 'theme';
+  
+  .element {
+    color: black;
+  }
+`)).to.deep.equal({}));
+
+it('should trim variable names', () => expect(lessVarsToJS(`
+    @blue : #0d3880;
+`)).to.deep.equal({
+  '@blue': '#0d3880'
+}));
+
+it('should trim values', () => expect(lessVarsToJS(`
+  @blue :   #0d3880  ;
+`)).to.deep.equal({
+  '@blue': '#0d3880'
+}));
+
+it('should read variables that are url', () => expect(lessVarsToJS(`
+  @icon-url : "https://xxx.com:8080/t/font";
+`)).to.deep.equal({
+  '@icon-url': 'https://xxx.com:8080/t/font'
+}));
+
+it('should remove the @ when stripPrefix is true', () => expect(lessVarsToJS(`
+    @blue : #0d3880;
+`, { stripPrefix: true })).to.deep.equal({
+  'blue': '#0d3880'
+}));
+
+it('should use default variable values', () => expect(lessVarsToJS(`
+  @color : @blue;
+`, { resolveVariables: true, dictionary: { 'blue': '#0000FF' } })).to.deep.equal({
+  '@color': '#0000FF'
+}));
+
+it('should not parse functions', () => expect(lessVarsToJS(`
+  @color : darken(@blue, 20%);
+`)).to.deep.equal({
+  '@color': 'darken(@blue, 20%)'
+}));
+
+it('should support sass variables', () => expect(lessVarsToJS(`
+  $font-stack:    Helvetica, sans-serif;
+  $primary-color: #333;
+`)).to.deep.equal({
+  '$font-stack': 'Helvetica, sans-serif',
+  '$primary-color': '#333'
+}));
+
+it('should support sass variables with stripPrefix', () => expect(lessVarsToJS(`
+  $font-stack:    Helvetica, sans-serif;
+  $primary-color: #333;
+`, { stripPrefix: true })).to.deep.equal({
+  'font-stack': 'Helvetica, sans-serif',
+  'primary-color': '#333'
+}));
